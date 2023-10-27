270568017679032c129ebef1151efa25a17fc81d
Furkan Yakkan <furkan.yakkan@agu.edu.tr>
2023-10-27 13:00:35 +0300

    routes and npm instalization

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-var http = require("http"); // nome modules => http , fs , os 
-var fs = require("fs");
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+//routes
+  app.use("/products/:id", function (req, res) {
+    res.send("product details "+req.params.id);
+
+  });
+  app.use("/products", function (req, res) {
+    res.send("products");
+  });
+  app.use("/", function (req, res) {
+    res.send("homePage");
+  });
 
 
-var server = http.createServer((req, res) => {
-
-  if (req.url == "/") {
-
-    fs.readFile("index.html", (err,html)=> {
-      res.write(html);
-      res.end();
-        });
-
-  }
-   else if (req.url == "/products") {
-    fs.readFile("products.html", (err,html)=> {
-      res.write(html);
-      res.end();
-    });
-  }
-  else {
-    fs.readFile("404.html", (err,html)=> {
-      res.write(html);
-      res.end();
-    });
-  }
-
-});
-
-server.listen(3000, () => {
-  console.log("node.js server at port 3000");
-});
-
-
+  console.log("listening on port 3000");
+})
