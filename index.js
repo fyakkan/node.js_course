316df4236b9f649b474af54b92d0cb1e6e503868
Furkan Yakkan <furkan.yakkan@agu.edu.tr>
2023-11-04 14:09:28 +0300

    express.js lectures

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
-
+app.use(express.static('public'));
 app.set("view engine","ejs");
+app.use(express.static('node_modules'));
+
+
+  const data = [
+    {id:1, name: "iphone 14", price: 3000,isActive:true,imageUrl: "14.jpg"},
+    {id:2, name: "iphone 15", price: 4000,isActive:false,imageUrl: "15.jpeg"},
+    {id:3, name: "iphone 16", price: 5000,isActive:true,imageUrl: "16.jpg"},
+  ]
+
 
   //routes
   app.use("/products/:id", function (req, res) {
-    res.render("product-details");
+    const product = data.find( p => p.id == req.params.id);
+    res.render("product-details",product);
 
   });
   app.use("/products", function (req, res) {
-    res.render("products");
+    res.render("products", {
+      products: data
+    });
   });
   app.use("/", function (req, res) {
     res.render("index");
