ba1160e5f13bbfbe8c900f75ec9fa14e1a40db09
Furkan Yakkan <furkan.yakkan@agu.edu.tr>
2023-11-05 15:08:36 +0300

    DB Connection is added Project is done

---
+++ config.js
@@ -0,0 +1,10 @@
+const config ={
+  db: {
+    host:"localhost",
+    user:"root",
+    password:"furkan123",
+    database:"nodedb"
+  }
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/users");
+
 app.use(express.static('public'));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static('node_modules'));
 
 
-  const data = [
-    {id:1, name: "iphone 14", price: 3000,isActive:true,imageUrl: "14.jpg"},
-    {id:2, name: "iphone 15", price: 4000,isActive:false,imageUrl: "15.jpeg"},
-    {id:3, name: "iphone 16", price: 5000,isActive:true,imageUrl: "16.jpg"},
-  ]
 
+//routes
 
-  //routes
-  app.use("/products/:id", function (req, res) {
-    const product = data.find( p => p.id == req.params.id);
-    res.render("product-details",product);
+app.use(userRoutes);
 
-  });
-  app.use("/products", function (req, res) {
-    res.render("products", {
-      products: data
-    });
-  });
-  app.use("/", function (req, res) {
-    res.render("index");
-  });
-
-  app.listen(3000, () => {
-    console.log("listening on port 3000");
-  })
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+})
 
